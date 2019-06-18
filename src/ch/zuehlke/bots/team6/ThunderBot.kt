4d0699e1a523e34a2b2efa0d67d9e6726a8eb6b4
Trentini, Muriele <muriele.trentini@zuehlke.com>
2019-06-18 18:45:59 +0200

    second draft team 6

--- src/ch/zuehlke/bots/team6/ThunderBot.kt
+++ src/ch/zuehlke/bots/team6/ThunderBot.kt
@@ -1,24 +1,31 @@
 package ch.zuehlke.bots.team6
 
+import ch.zuehlke.helpers.Helper.absbearing
+import ch.zuehlke.helpers.Helper.normaliseBearing
 import robocode.AdvancedRobot
 import robocode.ScannedRobotEvent
 import java.awt.Color
 
 
 class ThunderBot : AdvancedRobot() {
+    private var centerX = 0.0
+    private var centerY = 0.0
+
     override fun run() {
         beautifyRobot()
+        centerX = battleFieldWidth / 2
+        centerY = battleFieldHeight / 2
+        setMaxVelocity(2.0)
 
         while (true) {
-            if (isInBorderArea()) {
+            if (!isInBorderArea()) {
                 moveOval()
             } else {
-                reverseMoveOval()
+                print("move to center. location is $x, $y")
+                goTo(centerX, centerY)
             }
-
         }
     }
-
 
     private fun isInBorderArea(): Boolean {
         return x < sentryBorderSize || x > battleFieldWidth - sentryBorderSize || y < sentryBorderSize || y > battleFieldHeight - sentryBorderSize
@@ -26,35 +33,14 @@ class ThunderBot : AdvancedRobot() {
 
     private fun moveOval() {
         setTurnLeft(90.0)
-        setAhead(100.0)
+        setAhead(200.0)
 
-        while (distanceRemaining > 0 && turnRemaining > 0) {
-            execute()
-        }
+        finishExecution()
 
         setTurnRight(45.0)
-        setAhead(150.0)
+        setAhead(250.0)
 
-        while (distanceRemaining > 0 && turnRemaining > 0) {
-            execute()
-        }
-    }
-
-
-    private fun reverseMoveOval() {
-        setTurnRight(90.0)
-        setAhead(100.0)
-
-        while (distanceRemaining > 0 && turnRemaining > 0) {
-            execute()
-        }
-
-        setTurnLeft(45.0)
-        setAhead(150.0)
-
-        while (distanceRemaining > 0 && turnRemaining > 0) {
-            execute()
-        }
+        finishExecution()
     }
 
     private fun beautifyRobot() {
@@ -63,6 +49,38 @@ class ThunderBot : AdvancedRobot() {
         setRadarColor(Color(220, 215, 230))
         setBulletColor(Color(190, 180, 30))
         setScanColor(Color(240, 210, 250))
+    }
+
+    private fun goTo(x: Double, y: Double) {
+        val dist = 20.0
+        val angle = Math.toDegrees(absbearing(getX(), getY(), x, y))
+        val r = turnTo(angle).toDouble()
+        setAhead(dist * r)
+        finishExecution()
+    }
+
+    private fun finishExecution() {
+        while (distanceRemaining > 0 && turnRemaining > 0) {
+            execute()
+        }
+    }
+
+    private fun turnTo(angle: Double): Int {
+        var ang: Double = normaliseBearing(heading - angle)
+        val dir: Int
+        when {
+            ang > 90 -> {
+                ang -= 180.0
+                dir = -1
+            }
+            ang < -90 -> {
+                ang += 180.0
+                dir = -1
+            }
+            else -> dir = 1
+        }
+        setTurnLeft(ang)
+        return dir
     }
 
     override fun onScannedRobot(e: ScannedRobotEvent?) {
